915bf998179799a94ec16f9708e9d6da0f7cee06
yilmazchef <yilmaz@mail.be>
2021-10-18 07:43:50 +0200

    cloud configuration is added.

--- src/main/java/com/stem/app/App.java
+++ src/main/java/com/stem/app/App.java
@@ -1,10 +1,13 @@
 package com.stem.app;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Bootcamp API", version = "1.0", description = "Bootcamp Information"))
 public class App implements CommandLineRunner {
 
     public static void main(String[] args) {
